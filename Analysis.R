e862f6af8e9f39820250231300fa4e61e4e55343
Sohaib Ashraf <sa25aci@herts.ac.uk>
2025-12-10 19:46:00 +0000

    Rewrote scatter plot and histogram sections for correct execution

--- Analysis.R
+++ Analysis.R
@@ -1,1 +1,35 @@
-asjhdgajhsgdh
+# Load data
+data <- read.csv("/Users/hadimac/Documents/Group Project/B154/school_scores.csv")
+
+# ----------------------------
+# Scatter Plot: Test Takers vs Math Score
+# ----------------------------
+
+plot(
+  data$Total.Test.takers, 
+  data$Total.Math,
+  main = "Relationship Between Total Test-takers and Total Math Score",
+  xlab = "Total Test-takers (number of students)",
+  ylab = "Total Math Score (points)",
+  pch = 19,
+  col = "blue"
+)
+
+# Add regression line
+abline(lm(Total.Math ~ Total.Test.takers, data = data),
+       col = "red",
+       lwd = 2)
+
+
+# ----------------------------
+# Histogram of Math Scores
+# ----------------------------
+
+hist(
+  data$Total.Math,
+  main = "Distribution of Total Math Scores",
+  xlab = "Total Math Score (points)",
+  col = "lightblue",
+  border = "black"
+)
+
