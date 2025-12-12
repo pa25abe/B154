f069fd1110b047c22ee6054eef881c72b27c4fc6
ka25abi@herts.ac.uk <ka25abi@herts.ac.uk>
2025-12-12 15:28:42 +0000

    Add scatter plot for test-takers vs math scores
    Added a scatter plot to visualize the relationship between total test-takers and total math scores, including custom axes and a regression line.

--- Analysis.R
+++ Analysis.R
@@ -1,6 +1,30 @@
 # Load Data
 data <- read.csv("school_scores.csv")
 
+# =====
+# Plot.
+# =====
+plot(data$Total.Test.takers, data$Total.Math,
+     main = "Relationship Between Total Test-takers and Total Math Score",
+     xlab = "Total Test-takers (number of students)",
+     ylab = "Total Math Score (points)",
+     pch = 19, col = "blue",
+     ylim = c(400, 600),
+     xlim = c(20000, 100000), 
+     yaxt = "n",
+     xaxt = "n"
+)
+
+axis(side = 2, at = seq(400, 600, by = 50))
+
+axis(side = 1,
+     at = c(20000, 40000, 60000, 80000, 100000),
+     labels = c("20k", "40k", "60k", "80k", "100k")
+)
+
+# Regression line
+abline(lm(Total.Math ~ Total.Test.takers, data = data), col = "red", lwd = 2)
+
 # =========================
 # Histogram of Math Scores.
 # =========================
@@ -8,7 +32,6 @@ data <- read.csv("school_scores.csv")
 min_score <- min(data$Total.Math)
 max_score <- max(data$Total.Math)
 
-# X-axis range 350 - 650.
 xlim_lower <- 350
 xlim_upper <- 650
 
